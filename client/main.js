3c64140799b133a4c55534903d196da2300bfc21
dani-mashasha <dmmashasha@gmail.com>
2021-11-28 14:28:55 +0200

    bank display

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ const myForm = document.getElementById("myForm") ;
 const search = document.getElementById("search");
 const matchList = document.getElementById("matchList");
 const select = document.getElementById("branchList")
+const bankDiv = document.getElementById("bankDiv");
 
 const selectBranches=(data) =>{
     select.innerHTML="";
@@ -41,13 +42,35 @@ const searchBanks = async (e)=>{
     .then(data => auto( data));
 }
 
+const displyBank = (bank) =>{
+    console.log(bank)
+    bankDiv.innerHTML = "";
+
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `${bank.Bank_Name[0]} - ${bank.Branch_Name[0]}` 
+
+
+    const p = document.createElement("p");
+    p.innerHTML = `מספר בנק: ${bank.Bank_Code[0]}<br>
+    כתובת : ${bank.Branch_Address[0]}, ${bank.City[0]} <br>
+    מיקוד: ${bank.Zip_Code[0]}<br>
+    טלפון: ${bank.Telephone[0]}<br>
+    פקס: ${!bank.Fax[0]?  "אין ":  bank.Fax[0]}<br>
+    גישה לנכים: ${bank.Handicap_Access[0]}<br>
+     ${bank.day_closed[0]? `הסניף סגור:${bank.day_closed[0]} ` : null }<br>`
+
+    bankDiv.append(h2, p)
+
+
+}
+
 myForm.addEventListener("submit",async (e) => {
     e.preventDefault()
     const bankName = search.value
     const branchCode = select.value
     await fetch(`http://localhost:3000/${bankName}/${branchCode}`)
     .then(response => response.json())
-    .then(data => console.log( data));
+    .then(data => displyBank( data));
 })
 
 
